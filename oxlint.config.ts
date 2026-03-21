ffdffc3d90fab5af2728f75b1325ecdc7da97e65
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-03-21 17:53:59 -0400

    Migrate linting from ESLint to oxlint

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -3,8 +3,4 @@ import {defineConfig} from 'oxlint';
 
 export default defineConfig({
   extends: all,
-  rules: {
-    'simple-import-sort/exports': 'off',
-    'simple-import-sort/imports': 'off',
-  },
 });
